6e53734aef8e589d6e572a7f3ad78363496e3944
chiemella <williamschiemelaod@gmail.com>
2023-09-07 22:13:35 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,10 +11,10 @@ int main(void)
 	long long int d;
 	float f;
 
-	prntf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
-	prntf("Size of a int: %lu byte(s)\n", (unsigned long)sizeof(b));
-	prntf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
-	prntf("Size of long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
-	prntf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
-	return (0);
+prntf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
+prntf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
+prntf("Size of a long int: %lu bytes(s)\n", (unsigned long)sizeof(c));
+prntf("Size of a long long int: %lu bytes(s)\n", (unsigned long)sizeof(d));
+prntf("Size of a float: %lu bytes(s)\n", (unsigned long)sizeof(f));
+return (0);
 }
